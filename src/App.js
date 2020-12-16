2ad5846bc8c03a94656238a0b125b56df57e1c79
Luis Tavares <luis.tavares@checkmarx.com>
2020-12-16 17:29:34 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
 import './App.css';
 
 function App() {
+    const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState(['worldwide']);
+    const [countryInfo, setCountryInfo] = useState({});
+    const [tableData, setTabledata] = useState([]);
+
+    useEffect(() => {
+        fetch("https://disease.sh/v3/covid-19/all")
+            .then((response) => response.json())
+            .then((data) => {
+                setCountryInfo(data);
+            });
+    }, []);
+
+    // useeffect - run a piece of code, when the component loads and not again
+    useEffect(() => {
+        // async -> send request and wait for it
+        const getCountriesData = async () =>{
+            await fetch("https://disease.sh/v3/covid-19/countries")
+                .then((response) => response.json())
+                .then((data) => {
+                    const countries = data.map((country) => ({
+                        name: country.country,
+                        value: country.countryInfo.iso2
+                    }));
+
+                    setTabledata(data);
+                    setCountries(countries);
+                });
+        };
+        getCountriesData();
+        }, []);
+
+    const onCountryChange = async (event) => {
+        const countryCode = event.target.value;
+        setCountry(countryCode);
+
+        const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+        await fetch(url)
+            .then(response => response.json()).then((data) => {
+                setCountry(countryCode);
+
+                // all data for the country response
+                setCountryInfo(data)
+            });
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="app__left">
+            <div className="app__header">
+                <h1>Covid-19 Tracker</h1>
+                <FormControl className="app__dropdown">
+                    <Select variant="outlined" onChange={ onCountryChange } value={country}>
+                        <MenuItem value="worldwide">Worldwide</MenuItem>
+                        {
+                            countries.map(contry => (
+                                <MenuItem value={contry.value}>{contry.name}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                </FormControl>
+            </div>
+
+            {/* stats of all*/}
+            <div className="app__stats">
+                <InfoBox
+                    title="Coronavirus Cases"
+                    cases={countryInfo.todayCases}
+                    total={countryInfo.cases}
+                />
+                <InfoBox
+                    title="Recovered"
+                    cases={countryInfo.todayRecovered}
+                    total={countryInfo.recovered}
+                />
+                <InfoBox
+                    title="Deaths"
+                    cases={countryInfo.todayDeaths}
+                    total={countryInfo.deaths}
+                />
+            </div>
+
+            {/*Maps element*/}
+            <Map/>
+        </div>
+        <Card className="app__right">
+            <CardContent>
+                {/* table */}
+                <h3>Live Cases by Country</h3>
+                <Table countries={tableData}/>
+
+                <h3>New Cases</h3>
+            </CardContent>
+        </Card>
     </div>
   );
 }
